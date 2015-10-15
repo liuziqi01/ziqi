96aecc698e47e29dfa9d62db3fc3436a3217b40d
liuziqi01 <liuziqi01@Ziqis-MacBook-Air.local>
2015-10-15 13:07:00 -0400

    routers

--- routes.js
+++ routes.js
@@ -4,4 +4,7 @@ module.exports = function(app) {
     app.get('/watch', handlers.watch);
 app.get('/play',handlers.play);
 app.get('/game',handlers.game);
+app.get('/weeby/magic',handlers.weeby);
+app.get('/weeby',handlers.weeby);
+app.get('/weeby/yes',handlers.game);
 }
